014ced913eec590324ccb560704793c1af813bf7
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-20 15:10:01 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// Descrizione:
+// Partendo dal markup HTML allegato creare uno slider di immagini in VueJS.
+
+// Bonus:
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+
+const images = [
+    {
+        src: 'img/01.jpg',
+        title: 'Svezia',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    },
+    {
+        src: 'img/02.jpg',
+        title: 'Svizzera',
+        text: 'Lorem ipsum',
+    },
+    {
+        src: 'img/03.jpg',
+        title: 'Gran Bretagna',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+        src: 'img/04.jpg',
+        title: 'Germania',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    },
+    {
+        src: 'img/05.jpg',
+        title: 'Paradise',
+        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    },
+];
